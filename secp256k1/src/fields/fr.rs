94c2137d72046fc56553f0949f456d6f68b83289
Nicholas Ward <npward@berkeley.edu>
2021-03-09 13:42:08 -0800

    fixed Fr

--- secp256k1/src/fields/fr.rs
+++ secp256k1/src/fields/fr.rs
@@ -1,13 +1,13 @@
 use ark_ff::{
-    biginteger::BigInteger256 as BigInteger,
-    fields::{FftParameters, Fp256, Fp256Parameters, FpParameters},
+    biginteger::BigInteger320 as BigInteger,
+    fields::{FftParameters, Fp320, Fp320Parameters, FpParameters},
 };
 
 pub struct FrParameters;
 
-pub type Fr = Fp256<FrParameters>;
+pub type Fr = Fp320<FrParameters>;
 
-impl Fp256Parameters for FrParameters {}
+impl Fp320Parameters for FrParameters {}
 impl FftParameters for FrParameters {
     type BigInt = BigInteger;
 
@@ -15,12 +15,13 @@ impl FftParameters for FrParameters {
 
     // TWO_ADIC_ROOT_OF_UNITY = GENERATOR^T
     // Encoded in Montgomery form, so the value here is (5^T)R mod q.
-    // 96880290535369799104859127461068109275753111126600080047934318051043430856655
+    // 95821856126790151842923920902042261089361346486553104461182584587228582258626
     const TWO_ADIC_ROOT_OF_UNITY: BigInteger = BigInteger([
-        0xe823f5b94e5b77cf,
-        0xcab86f88ecd9ebea,
-        0xd3d93fa52862700f,
-        0xd6304d1f577eeabe,
+        0xd8fdbe6bab1827c2,
+        0x55c9ffadb706d40d,
+        0xdae7a417e6a410dd,
+        0xd3d93fa528627010,
+        0x0,
     ]);
 }
 
@@ -31,22 +32,25 @@ impl FpParameters for FrParameters {
         0xbaaedce6af48a03b,
         0xfffffffffffffffe,
         0xffffffffffffffff,
+        0x0,
     ]);
 
-    // R = 2^256 mod q = 432420386565659656852420866394968145599
+    // R = 2^320 mod q = 7976748203231275684456616952498544216114824026705293737984
     const R: BigInteger = BigInteger([
+        0x0,
         0x402da1732fc9bebf,
         0x4551231950b75fc4,
         0x1,
         0x0,
     ]);
 
-    // R2 = (2^256)^2 mod q
+    // R2 = (2^320)^2 mod q
     const R2: BigInteger = BigInteger([
-        0x896cf21467d7d140,
-        0x741496c20e7cf878,
-        0xe697f5e45bcd07c6,
-        0x9d671cd581c69bc5,
+        0x1e004f504dfd7f79,
+        0x8fcf59774a052ea,
+        0x27c4120fc94e1653,
+        0x3c1a6191e5702644,
+        0x0,
     ]);
 
     // 57896044618658097711785492504343953926418782139537452191302581570759080747168
@@ -55,6 +59,7 @@ impl FpParameters for FrParameters {
         0x5d576e7357a4501d,
         0xffffffffffffffff,
         0x7fffffffffffffff,
+        0x0,
     ]);
 
     // T and T_MINUS_ONE_DIV_TWO, where MODULUS - 1 = 2^S * T
@@ -65,20 +70,23 @@ impl FpParameters for FrParameters {
         0xfaeabb739abd2280,
         0xffffffffffffffff,
         0x3ffffffffffffff,
+        0x0,
     ]);
 
-    // (T - 1) / 2 = 904625697166532776746648320380374280100293470930272690489102837043110636674
+    // (T - 1) / 2 = 904632097166532776746648320380374280100293470930272690489102837043110636674
     const T_MINUS_ONE_DIV_TWO: BigInteger = BigInteger([
         0x777fa4bd19a06c82,
         0xfd755db9cd5e9140,
         0xffffffffffffffff,
         0x1ffffffffffffff,
+        0x0,
     ]);
 
     // GENERATOR = 5
     // Encoded in Montgomery form, so the value here is 5R mod q.
-    // 2162101932828298284262104331974840727995
+    // 39883741016156378422283084762492721080574120133526468689920
     const GENERATOR: BigInteger = BigInteger([
+        0x0,
         0x40e4273feef0b9bb,
         0x5a95af7e9394ded5,
         0x6,
